c1c8bec5a119964bcb01ac7725ae4da43023d31a
dastan0k <143653667+dastan0k@users.noreply.github.com>
2023-12-03 17:00:38 +0600

    Add files via upload

---
+++ lastdigit.java
@@ -0,0 +1,9 @@
+public class lastdigit {
+    public static void main(String[] args) {
+        int number = 546;
+
+        int lastDigit = number%10;
+
+        System.out.println(lastDigit);
+    }
+}
