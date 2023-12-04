06fa98e968d4c1839ba0107f63b71640293a526d
dastan0k <143653667+dastan0k@users.noreply.github.com>
2023-12-04 13:03:24 +0600

    Add files via upload

---
+++ increasing.java
@@ -0,0 +1,12 @@
+public class increasing {
+    public static void main(String[] args) {
+        int six = 6;
+
+        six++;
+        six++;
+        six++;//напишите тут ваш код
+
+
+        System.out.println(six);
+    }
+}
