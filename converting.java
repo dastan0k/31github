313cf387becb213237bb50b819ecc687ff5d75d3
dastan0k <143653667+dastan0k@users.noreply.github.com>
2023-12-06 12:12:57 +0600

    Add files via upload

---
+++ converting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class converting {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String a = console.nextLine();
+        String b = console.nextLine();
+        String c = console.nextLine();
+
+        System.out.println(c);
+        System.out.println(b.toUpperCase());
+        System.out.println(a.toLowerCase());
+    }
+}
