ed41928e981364139c745c80b93bfc77e1f4a377
dastan0k <143653667+dastan0k@users.noreply.github.com>
2023-12-12 16:37:28 +0600

    Add files via upload

---
+++ whiledo.java
@@ -0,0 +1,10 @@
+public class whiledo {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки.";
+        int n = 0;
+        while (n<+100){
+            n++;
+            System.out.println(quote);
+        }
+    }
+}
