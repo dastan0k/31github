4db8485acf5a7ab5bb76270e73e14ce182fb65e6
dastan0k <143653667+dastan0k@users.noreply.github.com>
2023-12-14 12:39:12 +0600

    Add files via upload

---
+++ filledtriangle.java
@@ -0,0 +1,15 @@
+public class filledtriangle {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n<5){
+            int m =0;
+            while (m<10){
+                System.out.print("Q");
+                m++;
+            }
+            System.out.println();
+            n++;
+        }
+
+    }
+}
