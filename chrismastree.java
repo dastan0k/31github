aaacfbf70e7e78fabee21f15cbdb12585485db0d
dastan0k <143653667+dastan0k@users.noreply.github.com>
2023-12-20 11:59:32 +0600

    Add files via upload

---
+++ chrismastree.java
@@ -0,0 +1,12 @@
+public class chrismastree {
+    public static void main(String[] args) {
+        for (int i = 1; i<=10; i++){
+            int triangleCount = 0 + i;
+            for (int t =0; t <triangleCount; t++){
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+
+    }
+}
