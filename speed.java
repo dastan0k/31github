fd904d4ffea45bfe7e6f2b89165cb7477bd12c1a
dastan0k <143653667+dastan0k@users.noreply.github.com>
2023-12-22 15:25:03 +0600

    Add files via upload

---
+++ speed.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class speed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mc = scanner.nextInt();
+        System.out.println((int)Math.round(3.6 * mc));
+
+    }
+}
